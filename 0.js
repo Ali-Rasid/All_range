2fde85df25a60edc4192da4d664c07d2a251ba4b
Ali Rasid <devmin335@gmail.com>
2025-07-01 11:41:32 +0700

    Upload file 0.js

---
+++ 0.js
@@ -0,0 +1,17 @@
+let nama = "Cecilon";
+let hobby = "nonton anime,tidur";
+let umur = 21;
+let title = "Raja Iblis";
+
+  console.log("====✓✓✓======")
+  console.log("Nama   :" ,nama );
+  console.log("Hobby  :" ,hobby);
+  console.log("Umur   :" ,umur);
+    if (umur >= 18) {
+  console.log("Status : Dewasa");
+      } else {
+  console.log("Status : Masih bocil");
+}
+  console.log("Title  :" ,title);
+
+console.log("====✓✓✓=====")
